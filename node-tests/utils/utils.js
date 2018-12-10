76ee27c07310a9ff23fab4396553cd8ed2f50c9b
marinaysn <marinaysn@gmail.com>
2018-12-09 22:31:02 -0500

    Added function inArray with test

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -79,3 +79,23 @@ module.exports.getMiddle = s => {
     return strText[mid];
   }
 };
+
+
+
+module.exports.inArray = (array1,array2) =>{
+  let  results = [];
+  
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array2[j].includes(array1[i])) {
+        results.push(array1[i]);
+        j = array2.length;
+      }
+    }
+  }
+
+if (results.length > 0){
+  results.sort();
+}
+  return results;
+}
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -81,3 +81,33 @@ it("Returns the middle character of the word", () => {
   expect(p).toBe('t', `Expected \'t\' but got ${p}`);
 });
 
+// -- Test 8 --
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+// #Example 1: a1 = ["arp", "live", "strong"]
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+// returns ["arp", "live", "strong"]
+// #Example 2: a1 = ["tarp", "mice", "bull"]
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+// returns []
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+// In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+// Beware: r must be without duplicates.
+// Don't mutate the inputs.
+it("Return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2", () => {
+
+  //subtest 1
+  let  a1 = ["arp", "live", "strong"]
+ let a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+ let p = u_utils.inArray(a1, a2);
+  expect(p).toInclude('arp', 'live', 'strong', `Expected to include "arp", "live", "strong" but got ${p}`);
+  
+   //subtest 2
+   a1 = ["tarp", "mice", "bull"]
+  a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+  
+  p = u_utils.inArray(a1, a2);
+
+ // expect(p).toInclude('arp', 'live', 'strong', `Expected to include "arp", "live", "strong" but got ${p}`);
+  expect(p).toEqual("", `Expected to be  empty but got: ${p}`);
+});
